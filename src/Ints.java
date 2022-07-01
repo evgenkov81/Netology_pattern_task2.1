108c39eb5bf2067f3886d7317e121d702e5e5e1d
Evgenii Kovalev <47632219+evgenkov81@users.noreply.github.com>
2022-07-01 15:41:51 +0300

    rename multi

---
+++ src/Ints.java
@@ -0,0 +1,8 @@
+public interface Ints {
+    int sum(int arg0, int arg1);
+
+    int multi(int arg0, int arg1);
+
+    int pow(int a, int b);
+}
+
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(10, 14));
+        System.out.println(intsCalc.multi(23, 62));
+        System.out.println(intsCalc.pow(4, 11));
+    }
+}
